6ad601d03e7fc5ed27c9859272c0dee193c1344b
Samueltrex <samueldavid09020850040@gmail.com>
2026-05-09 13:41:50 +0100

    added contact portal

--- app/page.tsx
+++ app/page.tsx
@@ -897,7 +897,7 @@ return (
               <div className="text-center md:text-left">
                 <h1 className="text-4xl md:text-6xl font-black tracking-tighter leading-[0.9] mb-6 uppercase">
                   The Guardian Initiative <br/> <span className="text-yellow-500">for</span> Community <br />
-                  Development <span className="text-yellow-500">.</span>
+                  Development<span className="text-yellow-500">.</span>
                 </h1>
                 <p className="text-lg text-black max-w-2xl font-light border-l-4 border-yellow-500 pl-6 leading-relaxed">
                   Empowering vulnerable populations and strengthening community resilience through evidence-based research in Plateau State.
