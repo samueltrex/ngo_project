340bf494b73cfd2211afc36cb3482c03ac2e5b6f
Samueltrex <samueldavid09020850040@gmail.com>
2026-05-10 15:22:38 +0100

    added contact portal

--- app/page.tsx
+++ app/page.tsx
@@ -578,7 +578,7 @@ const ActivityCarousel = () => {
   }, [images.length]);
 
   return (
-    <div className="relative w-full h-[800px] overflow-hidden border-b-8 border-black">
+    <div className="relative w-full h-full overflow-hidden border-b-8 border-black">
       {images.map((src, index) => (
         <div
           key={index}
