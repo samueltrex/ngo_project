4ef8daa1a8ca28c9f91a2aaf546f1578cfa8cfcc
Samueltrex <samueldavid09020850040@gmail.com>
2026-05-11 10:36:30 +0100

    added contact portal

--- app/page.tsx
+++ app/page.tsx
@@ -1015,9 +1015,9 @@ return (
     </section>
 
     {/* 2. About Us & Our Model (Side by Side on Desktop) */}
-    <section className="py-20 px-6 max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-1">
-      <div>
-        <h2 className="text-4xl font-oswald uppercase border-b-4 border-yellow-500 inline-block mb-6 pb-2 text-center">About Us</h2>
+      <section className="py-20 px-6 max-w-7xl mx-auto grid grid-cols-1"> 
+       <div>
+        <h2 className="text-4xl font-oswald uppercase border-b-4 border-yellow-500 text-center">About Us</h2>
         <p className="text-base leading-relaxed text-gray-700 font-lato text-justify">
           The Guardian Initiative for Community Development (GICD) is a child-focused Nigerian charity that responds to and addresses humanitarian and development challenges affecting children. We strengthen protection systems, advance education and youth development, and improve the resilience of households and communities to achieve sustainable outcomes across both humanitarian and development contexts.
         </p>
